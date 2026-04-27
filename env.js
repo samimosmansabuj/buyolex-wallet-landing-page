eef971803f83bb62983ed7f11ffdfa02a5fbe6a9
Samim Osman Sabuj <samim.o.sabuj01@gmail.com>
2026-04-27 21:21:32 +0600

    update and pixel add

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 window.ENV = {
     // API_BASE_URL: "https://apitest.pencilwoodbd.org",
-    API_BASE_URL: "http://127.0.0.1:8000",
+    // API_BASE_URL: "http://127.0.0.1:8000",
+    API_BASE_URL: "https://buyolex.com",
     WHATSAPP_NUMBER: "8801810447799",
     PRODUCT_LANDING_PAGE_ID: "1010",
 
